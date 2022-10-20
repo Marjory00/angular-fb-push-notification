acb6a451451f2df52778838f8c43160b4c9d1b97
Marjory Marquez <samaterra@hotmail.com>
2022-10-20 14:34:25 -0400

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: " ",
-    authDomain: " ",
-    projectId: " ",
-    storageBucket: " ",
-    messagingSenderId: " ",
-    appId: "",
-    measurementId: "",
+    apiKey: "ADD HERE",
+    authDomain: "ADD HERE",
+    projectId: "ADD HERE",
+    storageBucket: "ADD HERE",
+    messagingSenderId: "ADD HERE",
+    appId: "ADD HERE",
+    measurementId: "ADD HERE",
   }
 };
